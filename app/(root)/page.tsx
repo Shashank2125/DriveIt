74faeac8e795921e16bdd0f53166efbcae5c7e63
Shashank2125 <138694950+Shashank2125@users.noreply.github.com>
2025-12-09 22:32:31 +0530

    comlpleted all the funtionality of landing page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,14 @@
+import ActionDropdown from "@/components/ActionDropdown";
 import Card from "@/components/Card";
 import Chart from "@/components/Chart";
 import FormattedDateTime from "@/components/FormattedDateTime";
+import Thumbnail from "@/components/Thumbnail";
 import { Separator } from "@/components/ui/separator";
 import { getFiles, getTotalSpaceUsed } from "@/lib/actions/file.action";
 import { convertFileSize, getUsageSummary } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { Models } from "node-appwrite";
 
 const Dashboard = async () => {
   const [files, totalSpace] = await Promise.all([
@@ -39,6 +42,32 @@ const Dashboard = async () => {
             </Link>
           ))}
         </ul>
+      </section>
+      <section className="dashboard-recent-file">
+        <h2 className="h3 xl:h2 text-light-100">Recent File uploaded</h2>
+        {files.documents.length > 0 ? (
+          <ul className="mt-5 flex flex-col gap-5">
+            {files.documents.map((file: Models.Document) => (
+              <Link
+                href={file.url}
+                target="_blank"
+                className="flex items-center gap-3"
+                key={file.$id}
+              >
+                <Thumbnail type={file.type} extension={file.extension} url={file.url} />
+                <div className="recent-file-details">
+                  <div className="flex flex-col gap-1">
+                    <p className="recent-file-name">{file.name}</p>
+                    <FormattedDateTime date={file.$createdAt} className="caption" />
+                  </div>
+                  <ActionDropdown file={file} />
+                </div>
+              </Link>
+            ))}
+          </ul>
+        ) : (
+          <p className="empty-list">No files uploaded</p>
+        )}
       </section>
     </div>
   );
