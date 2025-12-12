3078779475a2d6a1e45b9e72ac82bb34c69311a6
Shashank2125 <138694950+Shashank2125@users.noreply.github.com>
2025-12-12 23:34:31 +0530

    fixes

--- lib/appwrite/config.ts
+++ lib/appwrite/config.ts
@@ -1,7 +1,7 @@
 export const appwriteConfig = {
   // ✅ Public (safe)
   endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
-  projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
+  projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!,
 
   // ✅ Server-only (DO NOT prefix with NEXT_PUBLIC)
   databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
